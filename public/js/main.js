fd93594456ec58e8cdad084bbc121b02b4f61088
HenryGBC <henry.bravo89@gmail.com>
2016-10-24 22:05:29 -0430

    Neida gift

---
+++ public/js/main.js
@@ -0,0 +1,52 @@
+$(function() {
+    console.log( "ready!" );
+    
+    var i = 0;
+    var error = 0;
+    $('.btn').click(function() {
+        
+        if(i===0){
+            
+            $('.panel').removeClass('block');
+            $('.panel-1').addClass('block');
+            $('.hearts').addClass('block');
+            i++;
+        }else{
+            console.log(i);
+            console.log($("#correct"+i).is(':checked'))
+            if($("#correct"+i).is(':checked')){
+                $('.panel').removeClass('block');
+                $('.panel-'+(i+1).toString()).addClass('block');
+                i++;
+            }else{
+               // $('.content').addClass('animated shake');
+                error++;
+                $('.content').animateCss('shake');
+                $('#heart'+error).animateCss('bounceOutUp');
+                
+                
+                setTimeout(function(){
+                   $('#heart'+error).addClass('none'); 
+                    if(error===3){
+                        location.reload();
+                    }
+                }, 800);
+                
+               
+            }
+        }
+        
+        
+       
+    });
+    
+    
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            this.addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+        }
+    });
+});
